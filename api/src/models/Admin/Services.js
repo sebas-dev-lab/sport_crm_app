561f5be81cc14efe7b183b9f401de411f95369e6
sebas-dev-lab <lb.lescano.vs@gmail.com>
2021-02-07 19:06:07 -0300

    Service Model

---
+++ api/src/models/Admin/Services.js
@@ -0,0 +1,52 @@
+const { Schema, model, connection } = require("mongoose");
+var autoincrement = require("mongoose-auto-increment");
+autoincrement.initialize(connection);
+
+const serviceSchema = new Schema({
+  code: {
+    type: Number,
+    default: 0,
+    unique: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  resume: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  ranking: {
+    type: Number,
+    required: false,
+  },
+  reviews: {
+    type: Schema.Types.ObjectId,
+    ref: "Reviews",
+  },
+});
+
+serviceSchema.plugin(require("mongoose.autopopulate"));
+serviceSchema.plugin(autoincrement.plugin, {
+  model: "Service",
+  field: "code",
+  startAt: 1,
+  incrementBy: 1,
+});
+
+serviceSchema.plugin(findOrCreate);
+const Service = model("Service", serviceSchema);
+
+module.exports = Service;
