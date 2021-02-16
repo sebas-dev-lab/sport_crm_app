6d35265ac6ec1e200245f51b4893ff464eb3f803
sebas-dev-lab <lb.lescano.vs@gmail.com>
2021-02-16 17:29:54 -0300

    category CRUD

--- api/src/controllers/Admin/Category.js
+++ api/src/controllers/Admin/Category.js
@@ -11,3 +11,50 @@ exports.post_category = async (req, res) => {
   await category.save();
   return res.status(200).json({ msj: "ok", category: category });
 };
+
+// ** # Get all categories #
+exports.get_all_cateogies = (req, res) => {
+  Category.find().then((categories) => {
+    if (!categories) {
+      return res.status(400).json({ msj: "Categories could not be found" });
+    }
+    return res.status(200).json({ msk: "ok", categories: categories });
+  });
+};
+
+// ** # Get one category #
+exports.get_one_category = (req, res) => {
+  const { code } = req.params;
+  if (!code) return res.status(400).json({ msj: "Code is required" });
+  Category.findOne({ code: code }).then((category) => {
+    if (!category)
+      return res.status(400).json({ msj: "Category could not be found" });
+    return res.status(200).json({ msj: "ok", category: category });
+  });
+};
+
+// ** # Put category #
+exports.put_category = async (req, res) => {
+  const { code, title } = req.body;
+  if (!code || !title) return res.status(400).json({ msj: "Data is required" });
+  const cat = await Category.findOneAndUpdate({ code: code }, { title: title });
+  console.log(cat.title);
+  if (cat.title !== title) {
+    return res.status(200).json({ msj: "ok", cat: cat });
+  } else {
+    return res.status(400).json({ msj: "Category can not be modified" });
+  }
+};
+// ** # Delete category #
+exports.delete_category = (req, res) => {
+  const { code } = req.params;
+  Category.findOneAndDelete({ code: code }).then((cat) => {
+    Category.findOne({ code: code }).then((control) => {
+      console.log(control);
+      if (control) {
+        return res.status(400).json({ msj: "Category could not be deleted" });
+      }
+      return res.status(200).json({ msj: "ok" });
+    });
+  });
+};
--- api/src/routes/Admin/Category.js
+++ api/src/routes/Admin/Category.js
@@ -3,5 +3,8 @@ const route = express.Router();
 const controller = require("../../controllers/Admin/Category");
 
 route.post("/", controller.post_category);
-
+route.get("/", controller.get_all_cateogies);
+route.get("/:code", controller.get_one_category);
+route.put("/", controller.put_category);
+route.delete("/:code", controller.delete_category);
 module.exports = route;
